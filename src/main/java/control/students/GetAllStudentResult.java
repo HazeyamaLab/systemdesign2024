e4c75eedb70c6cea7602718b86b8f91d0670a5c3
mgn901 <mgn901+dev@gmail.com>
2024-06-17 01:44:36 +0000

    add: GetAllStudentコントロールの説明を追加

--- src/main/java/control/students/GetAllStudentResult.java
+++ src/main/java/control/students/GetAllStudentResult.java
@@ -3,12 +3,19 @@ package control.students;
 import java.util.List;
 import entity.Student;
 
+/**
+ * {@link CreateStudent#execute(GetAllStudent)}の結果の出力に用いるオブジェクト。
+ * システムに登録されている学生の情報の一覧を持つ。
+ */
 public class GetAllStudentResult {
-	public final List<Student> students;
-	public final String message;
+  // このクラスから作成されたオブジェクトはコントローラへの入力の際に一度用いられるのみなので、各フィールドが変更可能である必要はない。
+  // そこで、`private`修飾子付きのフィールドとゲッターメソッドを定義する代わりに、
+  // 各フィールドに`final`修飾子を付けることで、読み取り専用の変更不可能なフィールドを定義している。
 
-	public GetAllStudentResult(List<Student> students, String message) {
+	/** システムに登録されているすべての学生の情報のリスト。 */
+	public final List<Student> students;
+
+	public GetAllStudentResult(List<Student> students) {
 		this.students = students;
-		this.message = message;
 	}
 }
