7e73aae6aa84652916483d6162c3e31b22dda71d
mgn901 <mgn901+dev@gmail.com>
2024-06-18 04:09:35 +0000

    change: ConnectionCloserとDataSourceHolderはdaoパッケージ以外から利用不可にする。

--- src/main/java/dao/DataSourceHolder.java
+++ src/main/java/dao/DataSourceHolder.java
@@ -5,7 +5,32 @@ import javax.sql.DataSource;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
-public class DataSourceHolder {
+/**
+ * DAOで利用する{@link javax.sql.DataSource}を提供する。
+ * 
+ * このクラスのインスタンスをDAOの{@code private}フィールドに割り当てておくことで、DAOの各メソッドで接続を取得する処理を記述しやすくなる。
+ * 
+ * <pre>{@code
+ * class SampleDao {
+ *   // `DataSourceHolder.dataSource`をDAOのフィールドに割り当てておく。
+ *   private final DataSource dataSource;
+ *   public SampleDao() {
+ *     this.dataSourceHolder = new DataSourceHolder().dataSource;
+ *   }
+ *   public List&lt;SampleEntity&gt; getAll() {
+ *     Connection connection = null;
+ *     try {
+ *       // `dataSource`フィールドに割り当てた`DataSource`オブジェクトの`getConnection`メソッドを呼び出して`java.sql.Connection`型のオブジェクトを得る。
+ *       connection = this.dataSource.getConnection();
+ *       // 何らかの処理
+ *     } catch (SQLException exception) {
+ *       // 何らかの処理
+ *     }
+ *   }
+ * }
+ * }</pre>
+ */
+class DataSourceHolder {
   private static HikariConfig _hikariConfig;
   private static DataSource _dataSource;
 
